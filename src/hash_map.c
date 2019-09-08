9e0f1b46e79b907f3c21e7508334d8303cb61bba
aegorenkov <aegorenkov.91@gmail.com>
2019-09-09 00:26:45 +0200

    hash map implementation and tests

---
+++ src/hash_map.c
@@ -0,0 +1,167 @@
+#include <hash_map.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LOAD_FACTOR 0.75f
+#define MINIMUM_CAPACITY 12
+#define GROW_FACTOR 2
+
+struct HashMap *create_hash_map(map_size_t (*hash_f) (void *),
+                                bool (*eq_f) (void *, void *),
+                                map_size_t initial_capacity) {
+  struct HashMap *map = malloc(sizeof(struct HashMap));
+  if (map == NULL) {
+    return NULL;
+  }
+  if (initial_capacity < MINIMUM_CAPACITY) {
+    initial_capacity = MINIMUM_CAPACITY;
+  }
+  map->hash_f = hash_f;
+  map->eq_f = eq_f;
+  map_size_t buckets_number = 16;
+  while (buckets_number * 0.75 < initial_capacity) {
+    buckets_number *= 2;
+  }
+  map->capacity = buckets_number * LOAD_FACTOR;
+  if (map->capacity < initial_capacity) {
+    printf("hash_map.c:19. create_hash_map. failed to determine number of buckets\n");
+    printf("initial_capacity: %lu\n", initial_capacity);
+    printf("LOAD_FACTOR: %.10f\n", LOAD_FACTOR);
+    printf("buckets_number: %lu\n", buckets_number);
+    printf("capacity: %lu\n", map->capacity);
+    exit(-1);
+  }
+  map->buckets = malloc(buckets_number * sizeof(struct HashMapNode));
+  if (map->buckets == NULL) {
+    free(map);
+    return NULL;
+  }
+  for (size_t i = 0; i < buckets_number; i++) {
+    (map->buckets + i)->node=NULL;
+  }
+  map->buckets_number = buckets_number;
+  map->entries_number = 0;
+  return map;
+}
+
+void __hash_map_put_node__(struct HashMap *map, struct HashMapNode *node) {
+  void *key = node->key;
+  map_size_t index = map->hash_f(key) & map->buckets_number - 1;
+  struct HashMapBucket *bucket = map->buckets + index;
+  struct HashMapNode *next_node = bucket->node;
+  node->next = next_node;
+  bucket->node = node;
+  map->entries_number += 1;
+}
+
+void __hash_map_resize__(struct HashMap *map) {
+  struct HashMapBucket *current_buckets = map->buckets;
+  map_size_t current_buckets_number = map->buckets_number;
+  map_size_t new_buckets_number = GROW_FACTOR * current_buckets_number;
+  map_size_t new_capacity = new_buckets_number * LOAD_FACTOR;
+
+  map->buckets = malloc(new_buckets_number * sizeof(struct HashMapNode));
+  for (size_t i = 0; i < new_buckets_number; i++) {
+    (map->buckets + i)->node=NULL;
+  }
+  map->buckets_number = new_buckets_number;
+  map->entries_number = 0;
+  map->capacity = new_capacity;
+  for (size_t i = 0; i < current_buckets_number; i++) {
+    struct HashMapBucket *bucket = current_buckets + i;
+    struct HashMapNode *node = bucket->node;
+    struct HashMapNode *next_node;
+    while (node != NULL) {
+      next_node = node->next;
+      __hash_map_put_node__(map, node);
+      node = next_node;
+    }
+  }
+
+  free(current_buckets);
+}
+
+struct HashMapNode *__hash_map_get_node__(struct HashMap *map, void *key) {
+  map_size_t index = map->hash_f(key) & map->buckets_number - 1;
+  struct HashMapBucket *bucket = map->buckets + index;
+  struct HashMapNode *node = bucket->node;
+  while (node != NULL) {
+    if (map->eq_f(node->key, key)) {
+      return node;
+    }
+    node = node->next;
+  }
+  return NULL;
+}
+
+void *hash_map_put(struct HashMap *map, void *key, void *value) {
+  void *result = NULL;
+  struct HashMapNode *node = __hash_map_get_node__(map, key);
+  if (node != NULL) {
+    result = node->value;
+    node->value = value;
+    return result;
+  } else {
+    map_size_t index = map->hash_f(key) & map->buckets_number - 1;
+    struct HashMapBucket *bucket = map->buckets + index;
+    node = malloc(sizeof(struct HashMapNode));
+    node->key = key;
+    node->value = value;
+    node->next = bucket->node;
+    bucket->node = node;
+    map->entries_number += 1;
+    if (map->entries_number > map->capacity) {
+      __hash_map_resize__(map);
+    }
+    return NULL;
+  }
+}
+
+bool hash_map_contains(struct HashMap *map, void *key) {
+  struct HashMapNode *node = __hash_map_get_node__(map, key);
+  return node != NULL;
+}
+
+void *hash_map_get(struct HashMap *map, void *key) {
+  struct HashMapNode *node = __hash_map_get_node__(map, key);
+  return node != NULL ? node->value : NULL;
+}
+
+void *hash_map_remove(struct HashMap *map, void *key) {
+  map_size_t index = map->hash_f(key) & map->buckets_number - 1;
+  struct HashMapBucket *bucket = map->buckets + index;
+  struct HashMapNode *prev = NULL;
+  struct HashMapNode *node = bucket->node;
+  while (node != NULL) {
+    if (map->eq_f(node->key, key)) {
+      if (prev == NULL) {
+        bucket->node = node->next;
+      } else {
+        prev->next = node->next;
+      }
+      map->entries_number -= 1;
+      void *result = node->value;
+      free(node);
+      return result;
+    }
+    prev = node;
+    node = node->next;
+  }
+  return NULL;
+}
+
+void free_hash_map(struct HashMap *map) {
+  for (size_t i = 0; i < map->buckets_number; i++) {
+    struct HashMapNode *node = (map->buckets + i)->node;
+    while (node != NULL) {
+      struct HashMapNode *next_node = node->next;
+      free(node);
+      node = next_node;
+    }
+  }
+  free(map->buckets);
+  free(map);
+}
---
+++ test/hash_map_test.c
@@ -0,0 +1,289 @@
+#include <assert.h>
+#include <hash_map.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+map_size_t hash_f(int8_t *key) {
+  return *key % 100;
+}
+
+bool eq_f(int8_t *key1, int8_t *key2) {
+  return *key1 == *key2;
+}
+
+void test_create_hash_map() {
+  struct HashMap *map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 0);
+
+  for (size_t i = 0; i < map->buckets_number; i++) {
+    assert((map->buckets + i)->node == NULL);
+  }
+
+  free_hash_map(map);
+
+  map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                        (bool (*)(void *, void*)) eq_f,
+                        1000);
+
+  assert(map->capacity == 1536);
+  assert(map->buckets_number == 2048);
+  assert(map->entries_number == 0);
+
+  free_hash_map(map);
+}
+
+void hash_map_print(struct HashMap *map) {
+  printf("{\n");
+  for (int8_t i = 0; i < map->buckets_number; i++) {
+    struct HashMapBucket *bucket = map->buckets + i;
+    printf("{i: %d->", i);
+    struct HashMapNode *node = bucket->node;
+    while (node != NULL) {
+      printf("{key: %d, value: %d}->", *(int8_t *) (node->key), *(int8_t *) (node->value));
+      node=node->next;
+    }
+    printf("NULL\n");
+  }
+  printf("}\n");
+}
+
+void test_hash_map_put() {
+  struct HashMap *map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+  int8_t key0 = 0;
+  int8_t key3 = 3;
+  int8_t key1 = 1;
+  int8_t key101 = 101;
+  int8_t value0 = 0;
+  int8_t value3 = 3;
+  int8_t value1 = 1;
+  int8_t value101 = 101;
+
+  hash_map_put(map, &key0, &value0);
+  assert(map->buckets->node->key == &key0);
+  assert(map->buckets->node->value == &value0);
+  assert(map->buckets->node->next == NULL);
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 1);
+
+  hash_map_put(map, &key3, &value3);
+  assert((map->buckets + 3)->node->key = &key3);
+  assert((map->buckets + 3)->node->value = &value3);
+  assert((map->buckets + 3)->node->next == NULL);
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 2);
+
+  hash_map_put(map, &key1, &value1);
+  struct HashMapNode *node1 = (map->buckets + 1)->node;
+  assert(node1->key == &key1);
+  assert(node1->value == &value1);
+  assert(node1->next == NULL);
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 3);
+
+  hash_map_put(map, &key101, &value101);
+  struct HashMapNode *node101 = (map->buckets + 1)->node;
+  assert(node101->key == &key101);
+  assert(node101->value == &value101);
+  assert(node101->next == node1);
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 4);
+
+  free_hash_map(map);
+
+  map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+
+  int8_t *keys[12];
+  int8_t *values[12];
+  for (int8_t i = 0; i < 12; i++) {
+    int8_t *key = malloc(sizeof(int8_t));
+    int8_t *value = malloc(sizeof(int8_t));
+    *key = i + 16;
+    *value = i;
+    keys[i] = key;
+    values[i] = value;
+    hash_map_put(map, key, value);
+  }
+
+  assert(map->capacity == 12);
+  assert(map->buckets_number == 16);
+  assert(map->entries_number == 12);
+
+  struct HashMapNode *node16 = (map->buckets)->node;
+  assert(node16->key == keys[0]);
+  assert(node16->value == values[0]);
+  assert(node16->next == NULL);
+
+  for (int8_t i = 1; i < 12; i++) {
+    struct HashMapNode *node = (map->buckets + i)->node;
+    assert(node->key == keys[i]);
+    assert(node->value == values[i]);
+    assert(node->next == NULL);
+  }
+
+  for (int8_t i = 12; i < 16; i++) {
+    assert((map->buckets + i)->node == NULL);
+  }
+
+  int8_t key116 = 116;
+  int8_t value116 = 116;
+  hash_map_put(map, &key116, &value116);
+
+  assert(map->capacity == 24);
+  assert(map->buckets_number == 32);
+  assert(map->entries_number == 13);
+
+  for (int8_t i = 0; i < 16; i++) {
+    assert((map->buckets + i)->node == NULL);
+  }
+
+  assert((map->buckets + 16)->node == node16);
+  assert(node16->next->key == &key116);
+  assert(node16->next->value == &value116);
+  assert(node16->next->next == NULL);
+
+  for (int8_t i = 1; i < 12; i++) {
+    struct HashMapNode *node = (map->buckets + i + 16)->node;
+    assert(node->key == keys[i]);
+    assert(node->value == values[i]);
+    assert(node->next == NULL);
+  }
+
+  for (int8_t i = 28; i < 32; i++) {
+    assert((map->buckets + i)->node == NULL);
+  }
+
+  free_hash_map(map);
+
+  for (int8_t i = 0; i < 12; i++) {
+    free(keys[i]);
+    free(values[i]);
+  }
+}
+
+void test_hash_map_contains() {
+  struct HashMap *map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+
+  int8_t *keys[256];
+  int8_t *values[256];
+  for (int8_t i = -128; i < 127; i++) {
+    int8_t *key = malloc(sizeof(int8_t));
+    int8_t *value = malloc(sizeof(int8_t));
+    *key = i;
+    *value = i;
+    keys[i + 128] = key;
+    values[i + 128] = value;
+    hash_map_put(map, key, value);
+  }
+
+  for (int8_t i = -128; i < 126; i++) {
+    assert(hash_map_contains(map, keys[i + 128]));
+  }
+
+  int8_t key126 = 126;
+  assert(hash_map_contains(map, &key126));
+  int8_t key127 = 127;
+  assert(!hash_map_contains(map, &key127));
+
+  free_hash_map(map);
+
+  for (int8_t i = -128; i < 127; i++) {
+    free(keys[i + 128]);
+    free(values[i + 128]);
+  }
+}
+
+void test_hash_map_get() {
+  struct HashMap *map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+
+  int8_t *keys[256];
+  int8_t *values[256];
+  for (int8_t i = -128; i < 127; i++) {
+    int8_t *key = malloc(sizeof(int8_t));
+    int8_t *value = malloc(sizeof(int8_t));
+    *key = i;
+    *value = i;
+    keys[i + 128] = key;
+    values[i + 128] = value;
+    hash_map_put(map, key, value);
+  }
+
+  for (int8_t i = -128; i < 126; i++) {
+    assert(hash_map_get(map, keys[i + 128]) == values[i + 128]);
+  }
+
+  int8_t key126 = 126;
+  assert(* (int8_t *) hash_map_get(map, &key126) == 126);
+  int8_t key127 = 127;
+  assert(hash_map_get(map, &key127) == NULL);
+
+  free_hash_map(map);
+
+  for (int8_t i = -128; i < 127; i++) {
+    free(keys[i + 128]);
+    free(values[i + 128]);
+  }
+}
+
+void test_hash_map_remove() {
+  struct HashMap *map = create_hash_map((map_size_t (*)(void *)) hash_f,
+                                        (bool (*)(void *, void*)) eq_f,
+                                        0);
+
+  int8_t *keys[256];
+  int8_t *values[256];
+  for (int8_t i = -128; i < 127; i++) {
+    int8_t *key = malloc(sizeof(int8_t));
+    int8_t *value = malloc(sizeof(int8_t));
+    *key = i;
+    *value = i;
+    keys[i + 128] = key;
+    values[i + 128] = value;
+    hash_map_put(map, key, value);
+  }
+
+  assert(map->entries_number == 255);
+  assert(hash_map_get(map, keys[-1 + 128]) == values[-1 + 128]);
+  assert(hash_map_remove(map, keys[-1 + 128]) == values[-1 + 128]);
+  assert(map->entries_number == 254);
+
+  free_hash_map(map);
+
+  for (int8_t i = -128; i < 127; i++) {
+    free(keys[i + 128]);
+    free(values[i + 128]);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  printf("Starting test execution.\n");
+  test_create_hash_map();
+  printf("test_create_hash_map passed.\n");
+  test_hash_map_put();
+  printf("test_hash_map_put passed.\n");
+  test_hash_map_contains();
+  printf("test_hash_map_contains passed.\n");
+  test_hash_map_get();
+  printf("test_hash_map_get passed.\n");
+  test_hash_map_remove();
+  printf("test_hash_map_remove passed.\n");
+  printf("Test execution finished.\n");
+  return 0;
+}
