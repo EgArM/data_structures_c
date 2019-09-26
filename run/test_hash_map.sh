a50ed7a4cea5f1dc7d9a734ce69657d8e11e740b
aegorenkov <aegorenkov.91@gmail.com>
2019-09-26 20:58:40 +0200

    wall werr added to test runners

--- run/test_hash_map.sh
+++ run/test_hash_map.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gcc -I../header/ -o hash_map_test ../src/hash_map.c ../test/hash_map_test.c
+gcc -Wall -Werror -I../header/ -o hash_map_test ../src/hash_map.c ../test/hash_map_test.c
 ./hash_map_test
 rm hash_map_test
